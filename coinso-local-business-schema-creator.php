02a310fc6feb22712a20ae6b4613ae919ea429c6
ido barnea <barbareshet@gmail.com>
2017-02-28 12:38:32 +0200

    first commit
    create local business schema and add it with shortcode to your page

---
+++ coinso-local-business-schema-creator.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: coinso-local-business-schema-creator
+Plugin URI: https://github.com/coinso/coinso-local-business-schema-creator
+Description: create your local schema and add to pages and posts with shortcode
+Version: 0.1.0
+Author: ido @coinso.com
+Author URI: http://coinso.com
+Text Domain: clbsc
+Domain Path: /languages
+*/
+
+if( !defined('ABSPATH') ){
+    exit();
+}
